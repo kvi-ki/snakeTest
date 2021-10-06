c8b93355f7c0cb5416702792631e58d58a8db8a1
Victoria Kovaleva <victoria.kovaleva@accenture.com>
2021-10-06 15:50:03 +0200

    add players section

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const player1 = document.querySelector('.player-0');
+const player2 = document.querySelector('.player-1');
 const btnRoll = document.querySelector('.button');
 const snake = document.querySelector('.snake');
 const allCells = document.querySelectorAll('.cell');
@@ -10,13 +12,19 @@ const closeBtn = document.querySelector('.close');
 let dice = document.querySelector('.dice-image');
 let number = 0;
 
+let playing = true;
+
+player1.classList.add('player-active');
+
 btnRoll.addEventListener('click', function() {
-    let id = findSnakeId();
-    number = Math.trunc(Math.random() * 6) + 1;
-    dice.classList.remove('hidden');
-    dice.src = `./assets/dice-${number}.png`;
-    dice.classList.toggle('rotate');
-    moveSnake(id);     
+    if (playing) {
+        let id = findSnakeId();
+        number = Math.trunc(Math.random() * 6) + 1;
+        dice.classList.remove('hidden');
+        dice.src = `./assets/dice-${number}.png`;
+        dice.classList.toggle('rotate');
+        moveSnake(id); 
+    }    
 });
 
 function findSnakeId() {
@@ -40,12 +48,14 @@ function moveSnake(x) {
 
 function checkWinner(id) {
     if (id === 100) {
+        playing = false;
         winnerPopUp.classList.remove('hidden');
         document.querySelector('.main').classList.add('overlay');
     }
 }
 
 function closeWinnerPopUp() {
+    playing = true;
     winnerPopUp.classList.add('hidden');
     dice.classList.add('hidden');
     document.getElementById('1').appendChild(snake);
